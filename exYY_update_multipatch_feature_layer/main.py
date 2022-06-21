1a02cf17c106002256294d408662278979b02b2c
Simon Haegler <shaegler@esri.com>
2022-06-21 18:18:26 +0200

    wip arcpy

--- exYY_update_multipatch_feature_layer/main.py
+++ exYY_update_multipatch_feature_layer/main.py
@@ -1,21 +1,21 @@
+import keyring
 import shutil
 import tempfile
 from pathlib import Path
-import getpass
 
 import arcgis.features
-import keyring
-import requests
 from arcgis.gis import GIS
 import pyprt
 from pyprt.pyprt_arcgis import arcgis_to_pyprt
 
-from gdal import ogr
+import arcpy
+
+#from gdal import ogr
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 
 TARGET_GDB_TEMPLATE_PATH = SCRIPT_DIR.joinpath('pyprt_update_test_target.gdb')
-GDB_DRIVER = ogr.GetDriverByName("FileGDB")
+#GDB_DRIVER = ogr.GetDriverByName("FileGDB")
 
 SHAPE_BUFFER_ENCODER_ID = 'com.esri.prt.codecs.ShapeBufferEncoder'
 MULTIPATCH_DATA_FILE = "-1_model"  # don't ask ;-)
@@ -51,13 +51,19 @@ def update_multipatch_feature_layer():
     temp_filegdb_path = temp_dir.joinpath('temp.gdb')
     shutil.copytree(TARGET_GDB_TEMPLATE_PATH, temp_filegdb_path)
 
-    gdb = GDB_DRIVER.Open(str(temp_filegdb_path), 0)
-    multipatch_layer = gdb.GetLayerByName("pyprt_update_test_ProcedurallyGeneratedMultipatches")
-    print(multipatch_layer)
+    # gdb = GDB_DRIVER.Open(str(temp_filegdb_path), 0)
+    # multipatch_layer = gdb.GetLayerByName("pyprt_update_test_ProcedurallyGeneratedMultipatches")
+    # print(multipatch_layer)
 
-    temp_multipatch_data_path = temp_dir.joinpath(MULTIPATCH_DATA_FILE)
+    arcpy.env.workspace = str(temp_filegdb_path)
+    dataset_list = arcpy.ListFeatureClasses("*")
+    for dataset in dataset_list:
+        with arcpy.da.SearchCursor(dataset, "*") as cur:
+            for row in cur:
+                print(row)
 
     # workaround: generate features one-by-one to avoid overwriting the resulting multipatch data files
+    temp_multipatch_data_path = temp_dir.joinpath(MULTIPATCH_DATA_FILE)
     for source_feature in source_features:
         oid = source_feature.get_value('objectid')
         pyprt_initial_shapes = arcgis_to_pyprt(arcgis.features.FeatureSet([source_feature]))
@@ -68,18 +74,15 @@ def update_multipatch_feature_layer():
             multipatch_data = multipatch_file.read()
             print(f"read multipatch: {len(multipatch_data)} bytes")
 
-        gdb_feature = multipatch_layer.GetNextFeature()
-        gdb_feature_geo = gdb_feature.GetGeometryRef()
-        gdb_feature.SetGeometry(multipatch_data)
+        # gdb_feature = multipatch_layer.GetNextFeature()
+        # gdb_feature_geo = gdb_feature.GetGeometryRef()
+        # gdb_feature.SetGeometry(multipatch_data)
 
-
-
-    target_feature_layer_collection = gis.content.get(target_feature_layer_id)
-    target_feature_layer = target_feature_layer_collection.layers[0]
+    # target_feature_layer_collection = gis.content.get(target_feature_layer_id)
+    # target_feature_layer = target_feature_layer_collection.layers[0]
 
     #fgdb_upload_id = gis.content.add(data=)
     #target_feature_layer.append(item_id=fgdb_upload_id, upsert=True, update_geometry=True, upload_format='filegdb', upsert_matching_field='objectid', source_table_name="pyprt_tests", return_messages=True)
-
 
     pyprt.shutdown_prt()
     temp_dir.cleanup()
